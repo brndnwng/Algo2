e6d722bc3c26542222cda77fc8d31d7d2b1fc30d
Brandon Wong <softprac@gmail.com>
2013-09-07 12:19:06 -0400

    Job object for assignment 1

---
+++ Algo2/src/algo2/programassignment/one/Job.java
@@ -0,0 +1,20 @@
+package algo2.programassignment.one;
+
+public class Job {
+
+	private final int weight;
+	private final int length;
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public Job(int weight, int length) {
+		this.weight = weight;
+		this.length = length;
+	}
+}
